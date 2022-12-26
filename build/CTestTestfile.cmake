856dad2fcb2da91726a49d77c7cb48a2bce4ebb3
Jesper Ekstrøm <jesperoka@gmail.com>
2022-12-26 12:41:55 +0100

    catkin_make

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jesperek/Documents/school/CMR-Project-2022-2023/src
+# Build directory: /home/jesperek/Documents/school/CMR-Project-2022-2023/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
