3f9f422713f2ec3d7a45b9e9b7eb8d1962a221eb
Jesper Ekstrøm <jesperoka@gmail.com>
2022-12-26 13:03:46 +0100

    .

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jesperek/Documents/school/CMR-Project-2022-2023/src
-# Build directory: /home/jesperek/Documents/school/CMR-Project-2022-2023/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
